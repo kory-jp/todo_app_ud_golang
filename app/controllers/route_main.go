7f2453b30955cf74d3b8726fc68226550b0a5b2c
kory-job <catcher.intherye.rk@gmail.com>
2021-10-29 22:20:56 +0900

    auth/redirect

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -13,8 +13,21 @@ func top(w http.ResponseWriter, r *http.Request) {
 	// }
 	// Executeにてテンプレートファイルを実行,第二引数にはデータの引き渡し
 	// t.Execute(w, "Hello")
+	_, err := session(w, r)
+	if err != nil {
+		// 引数には表示するHTMLパーツ、データを一覧揃える
+		generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
 
-	// 引数には表示するHTMLパーツ、データを一覧揃える
-	generateHTML(w, "Hello", "layout", "public_navbar", "top")
-
+func index(w http.ResponseWriter, r *http.Request) {
+	// sessionを取得できればindex,sessionが返ってこなければTOP
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "index")
+	}
 }
